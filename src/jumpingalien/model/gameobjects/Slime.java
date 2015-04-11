431873e5b89165da46f76e4636b8577a61653f16
Vincent Kemps <kempsvincent@gmail.com>
2015-04-11 22:16:34 +0200

    Fixed the errors in Slime.

--- src/jumpingalien/model/gameobjects/Slime.java
+++ src/jumpingalien/model/gameobjects/Slime.java
@@ -1,5 +1,30 @@
 package jumpingalien.model.gameobjects;
 
+import jumpingalien.model.worldfeatures.World;
+import jumpingalien.util.Sprite;
+
 public class Slime extends Character{
+	
+	public Slime(int x, int y, Sprite[] sprites, School school){
+		super(x,y,0,2.5,sprites);
+	}
+	
+	@Override
+	protected boolean isValidWorld(World world) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void updatePosition(double timeDuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateSpriteIndex() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
