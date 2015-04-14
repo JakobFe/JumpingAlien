73d32029a73cd78c13af145f422b23c858a8c6f1
Jakob Festraets <jaapjop95@gmail.com>
2015-04-14 14:10:57 +0200

    First implementation of Slime.

--- src/jumpingalien/model/gameobjects/Slime.java
+++ src/jumpingalien/model/gameobjects/Slime.java
@@ -1,30 +1,62 @@
 package jumpingalien.model.gameobjects;
 
+import jumpingalien.model.exceptions.IllegalTimeIntervalException;
+import jumpingalien.model.exceptions.IllegalXPositionException;
+import jumpingalien.model.exceptions.IllegalYPositionException;
 import jumpingalien.model.worldfeatures.World;
 import jumpingalien.util.Sprite;
+import be.kuleuven.cs.som.annotate.Basic;
+import be.kuleuven.cs.som.annotate.Immutable;
+import be.kuleuven.cs.som.annotate.Model;
 
 public class Slime extends Character{
 	
 	public Slime(int x, int y, Sprite[] sprites, School school){
-		super(x,y,0,2.5,sprites);
+		super(x,y,0,SLIME_VELOCITY,sprites);
 	}
 	
 	@Override
 	protected boolean isValidWorld(World world) {
-		// TODO Auto-generated method stub
-		return false;
+		return (world == null || world.hasAsSlime(this));
 	}
+	
+	/**
+	 * Return the horizontal velocity of this slime.
+	 */
+	@Override
+	public final double getHorVelocity(){
+		return SLIME_VELOCITY;
+	}
+	
+	/**
+	 * A variable storing the velocity of a slime.
+	 * Its value is always 2.5 m/s.
+	 */
+	private static final double SLIME_VELOCITY = 2.5;
+
+	/**
+	 * Return the maximum horizontal acceleration of the Slime.
+	 */
+	@Basic @Immutable @Model @Override
+	protected double getMaxHorAcceleration(){
+		return maxHorAcceleration;
+	}
+	
+	/**
+	 * A variable storing the maximum horizontal acceleration for this Slime.
+	 * This variable must always store a positive number of type double or zero.
+	 */
+	private static final double maxHorAcceleration = 0.7;
+
+	
+	@Override
+	public void advanceTime(double timeDuration) throws IllegalXPositionException,
+	IllegalYPositionException,IllegalTimeIntervalException{}
+	
+	@Override
+	protected void updatePosition(double timeDuration) {}
 
 	@Override
-	protected void updatePosition(double timeDuration) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void updateSpriteIndex() {
-		// TODO Auto-generated method stub
-		
-	}
+	public void updateSpriteIndex() {}
 	
 }
--- src/jumpingalien/model/worldfeatures/World.java
+++ src/jumpingalien/model/worldfeatures/World.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import jumpingalien.model.exceptions.*;
 import jumpingalien.model.gameobjects.Mazub;
 import jumpingalien.model.gameobjects.Plant;
+import jumpingalien.model.gameobjects.Slime;
 import be.kuleuven.cs.som.annotate.*;
 import static jumpingalien.tests.util.TestUtils.intArray;
 
@@ -273,6 +274,35 @@ public class World {
 	}
 
 	private final HashSet<Plant> allPlants = new HashSet<Plant>();
+
+	public HashSet<Slime> getAllSlimes() {
+		return allSlimes;
+	}
+	
+	public HashSet<Slime> getAllUnterminatedSlimes(){
+		HashSet<Slime> result = new HashSet<Slime>();
+		for(Slime slime: getAllSlimes()){
+			if(!slime.isTerminated())
+				result.add(slime);
+		}	
+		return result;	
+	}
+	
+	public boolean hasAsSlime(Slime slime){
+		return getAllSlimes().contains(slime);
+	}
+	
+	public void addAsSlime(Slime slime){
+		getAllSlimes().add(slime);
+		slime.setWorld(this);
+	}
+	
+	public void removeAsSlime(Slime slime){
+		assert hasAsSlime(slime);
+		allSlimes.remove(slime);
+	}
+
+	private final HashSet<Slime> allSlimes = new HashSet<Slime>();
 	
 	public boolean didPlayerWin(){
 		if (getMazub() == null)
