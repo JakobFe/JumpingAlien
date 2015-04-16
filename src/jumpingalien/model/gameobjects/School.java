f0d04e6f4aba2e83854f4a72e7da60b1369c5e89
Vincent Kemps <kempsvincent@gmail.com>
2015-04-16 18:08:05 +0200

    Further implementation of slime. Almost all methods were copied from shark: maybe we should add an extra abstract class 'uncontrollable' under character (periodDuration,randomDirection,advanceTime...).

--- src/jumpingalien/model/gameobjects/School.java
+++ src/jumpingalien/model/gameobjects/School.java
@@ -1,5 +1,28 @@
 package jumpingalien.model.gameobjects;
 
-public class School {
+import java.util.HashSet;
 
+public class School {
+	
+	public School(){}
+	
+	int getNbSlimes(){
+		return slimes.size();
+	}
+	
+	void addSlime(Slime slime){
+		slimes.add(slime);
+		slime.setSchool(this);
+	}
+	
+	boolean hasAsSlime(Slime slime){
+		return slimes.contains(slime);
+	}
+	
+	void removeSlime(Slime slime){
+		assert hasAsSlime(slime);
+		slimes.remove(slime);
+	}
+	
+	private HashSet<Slime> slimes = new HashSet<Slime>();
 }
