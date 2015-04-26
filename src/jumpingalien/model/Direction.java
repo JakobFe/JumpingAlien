4aa3f43e414f5df7a7ee3d45dbe953fb37e37248
Vincent Kemps <kempsvincent@gmail.com>
2015-04-26 17:05:22 +0200

    Added documentation to Direction.

--- src/jumpingalien/model/Direction.java
+++ src/jumpingalien/model/Direction.java
@@ -4,14 +4,26 @@ public enum Direction {
 	
 	RIGHT(1), LEFT(-1), UP(1), DOWN(-1), NULL(0);
 	
+	/**
+	 * Initialize this direction with a given factor.
+	 * 
+	 * @param 	factor
+	 * 			The factor for this direction.
+	 */
 	private Direction(int factor){
 		this.factor = factor;
 	}
 	
+	/**
+	 * Returns the factor associated with this direction.
+	 */
 	public int getFactor() {
 		return factor;
 	}
 	
+	/**
+	 * A variable storing the factor belonging to this direction.
+	 */
 	private final int factor;
 
 }
