199c584442324d32220bfbb7b376d60d97b01b9e
Vincent Kemps <kempsvincent@gmail.com>
2015-04-26 17:41:29 +0200

    Finished documentation of position.

--- src/jumpingalien/model/Position.java
+++ src/jumpingalien/model/Position.java
@@ -8,6 +8,28 @@ import static jumpingalien.tests.util.TestUtils.*;
 @Value
 public class Position {
 	
+	/**
+	 * Initialize this position with a given x and y position and a given world.
+	 * 
+	 * @param 	x
+	 * 			The x position for this new position.
+	 * @param 	y
+	 * 			The y position for this new position.	
+	 * @param 	world
+	 * 			The world belonging to this new position.
+	 * @post	...
+	 * 			| new.getXPosition() == x
+	 * @post	...
+	 * 			| new.getYPosition() == y
+	 * @effect	...
+	 * 			| setWorld(world)
+	 * @throws 	IllegalXPositionException
+	 * 			...
+	 * 			| isValidXPosition(x,world) 
+	 * @throws 	IllegalYPositionException
+	 * 			...
+	 * 			| isValidYPosition(y,world)
+	 */
 	public Position(double x, double y,World world) throws 
 	IllegalXPositionException,IllegalYPositionException{
 		setWorld(world);
@@ -15,7 +37,15 @@ public class Position {
 		setYPosition(y);
 	}
 	
-	
+	/**
+	 * Initialize this position with a given x and y position and a no world.
+	 * @param 	x
+	 * 			The x position for this new position.
+	 * @param 	y
+	 * 			The y position for this new position.	
+	 * @effect	...
+	 * 			| this(x,y,null)
+	 */
 	public Position(double x, double y) throws
 	IllegalXPositionException,IllegalYPositionException{
 		this(x,y,null);
@@ -23,15 +53,15 @@ public class Position {
 	
 	
 	/**
-	 * Return the effective x-position of this Mazub.
+	 * Return the x component of this position.
 	 */
-	@Basic@Immutable@Model
+	@Basic@Immutable
 	public double getXPosition(){
 		return this.xPosition;
 	}
 	
 	/**
-	 * Return the displayed x position of this Mazub.
+	 * Return the displayed x component of this position.
 	 */
 	public int getDisplayedXPosition(){
 		return (int) Math.floor(getXPosition());
@@ -42,12 +72,16 @@ public class Position {
 	 * 
 	 * @param 	x
 	 * 			Effective x position to check
-	 * @return 	True if and only if the given x, rounded down to an integer number,
-	 * 			is a valid x-position.
-	 * 			| result == isValidXPosition( (int) Math.floor(x))
+	 * @param	world
+	 * 			The world to check against.
+	 * @return 	...
+	 * 			| if(world == null)
+	 * 			|	then result == true
+	 * 			| else
+	 * 			|	result == ((x >= 0) && ((int) Math.floor(x) < (world.getWorldWidth())))
 	 */
 	@Model
-	public static boolean isValidXPosition(double x, World world){
+	private static boolean isValidXPosition(double x, World world){
 		if (world == null)
 			return true;
 		else
@@ -59,34 +93,34 @@ public class Position {
 	 * 
 	 * @param	x
 	 * 			The new effective x position.
-	 * @post	The new effective x position of this Mazub is equal to the given x.
+	 * @post	...
 	 * 			| new.getEffectiveXPos() == x
-	 * @effect	The displayed x position is set to the effective x-coordinate,
-	 * 			rounded down to an integer number.
-	 * 			| setXPosition((int) Math.floor(x))
 	 * @throws	IllegalXPositionException((int) Math.floor(x),this)
-	 * 			The given x position is not a valid effective x position.
-	 * 			| !isValidEffectiveXPos(x)
+	 * 			...
+	 * 			| !isValidXPos(x,getWorld())
 	 */
 	@Model
-	public void setXPosition(double x) throws IllegalXPositionException{
+	private void setXPosition(double x) throws IllegalXPositionException{
 		if (!isValidXPosition(x,getWorld()))
 			throw new IllegalXPositionException((int) Math.floor(x));
 		this.xPosition = x;
 	}
 	
+	/**
+	 * A variable storing the x component of this position.
+	 */
 	private double xPosition;
 	
 	/**
-	 * Return the effective y-position of this Mazub.
+	 * Return the y component of this position.
 	 */
-	@Basic@Immutable@Model
+	@Basic@Immutable
 	public double getYPosition(){
 		return this.yPosition;
 	}
 	
 	/**
-	 * Return the y-position of this Mazub.
+	 * Return the displayed y component of this position.
 	 */
 	public int getDisplayedYPosition(){
 		return (int) Math.floor(getYPosition());
@@ -96,13 +130,17 @@ public class Position {
 	 * Check whether the given y is a valid y position.
 	 * 
 	 * @param 	y
-	 * 			y position to check
-	 * @return 	True if and only if y is not negative and smaller than
-	 * 			the screen height.
-	 * 			| result == (y >= 0) && (y < getScreenHeight())
+	 * 			y position to check.
+	 * @param	world
+	 * 			The world to check against.
+	 * @return 	...
+	 * 			| if(world == null)
+	 * 			|	then result == true
+	 * 			| else
+	 * 			|	result == (((y >= 0) && ((int) Math.floor(y) < world.getWorldHeight())))
 	 */
 	@Model
-	public static boolean isValidYPosition(double y, World world){
+	private static boolean isValidYPosition(double y, World world){
 		if (world == null)
 			return true;
 		else
@@ -114,48 +152,66 @@ public class Position {
 	 * 
 	 * @param	y
 	 * 			The new y position.
-	 * @post	The new y position of this Mazub is equal to the given y position.
+	 * @post	The new y component of this position is equal to the given y position.
 	 * 			| new.getYPosition() == y
-	 * @throws	IllegalYPositionException(y,this)
-	 * 			The given y position is not a valid y position.
-	 * 			| !isValidYPosition(y)
+	 * @throws	IllegalYPositionException((int) Math.floor(y))
+	 * 			| !isValidYPosition(y,getWorld())
 	 */
-	@Raw @Model
-	public void setYPosition(double y) throws IllegalYPositionException{
+	@Model
+	private void setYPosition(double y) throws IllegalYPositionException{
 		if (!isValidYPosition(y,getWorld()))
 			throw new IllegalYPositionException((int) Math.floor(y));
 		this.yPosition = y;
 	}
 	
 	/**
-	 * Variable storing the y position to be displayed.
-	 * 
-	 * The y position is an integer number and refers to the 
-	 * y-coordinate of the left bottom pixel of this Mazub 
-	 * in Cartesian coordinates. This variable does not store 
-	 * the effective y-coordinate of this Mazub, but the 
-	 * effective y-coordinate rounded down to an integer number.
-	 * This variable is only used in methods concerning the display
-	 * of this Mazub in the game world. For all other intentions,
-	 * the effective y position is used. 
+	 * Variable storing the y component of this position.
 	 */
 	private double yPosition = 0;	
 	
+	/**
+	 * A method to format this position into a double array.
+	 * 
+	 * @return	...
+	 * 			| result.length == 2
+	 * @return	...
+	 * 			| result[0] == getXPosition()
+	 * 			| result[1] == getYPosition()
+	 */
 	public double[] toDoubleArray(){
 		return doubleArray(this.getXPosition(),this.getYPosition());
 	}
 	
+	/**
+	 * Return the world belonging to this position.
+	 */
 	World getWorld(){
 		return this.world;
 	}
 	
+	/**
+	 * Sets the world of this position to the given world.
+	 * 
+	 * @param 	world
+	 * 			The world to set.
+	 * @post	...
+	 * 			| new.getWorld() == world
+	 */
 	private void setWorld(World world){
-		// nog precondities opleggen
 		this.world = world;
 	}
 	
+	/**
+	 * A variable storing the world belonging to this position.
+	 */
 	private World world;
 	
+	/**
+	 * A method to terminate this position.
+	 * 
+	 * @post	...
+	 * 			| new.getWorld() == null
+	 */
 	public void terminate(){
 		this.world = null;
 	}
@@ -167,8 +223,54 @@ public class Position {
 		return new Position(getXPosition(),getYPosition(),getWorld());
 	}
 	
+	/**
+	 * A method to check whether two positions are the same.
+	 * 
+	 * @return	...
+	 * 			| if(other == null || !(other instanceof Position))
+	 * 			|	then result == false
+	 * 			| else
+	 * 			| 	result ==
+	 * 			|		((this.getXPosition() == ((Position)other).getXPosition()) &&
+	 *			|  		(this.getYPosition() == ((Position)other).getYPosition()) &&
+	 *			|  		(this.getWorld() == ((Position)other).getWorld()))
+	 */
+	@Override
+	public boolean equals(Object other) {
+		if(other == null || !(other instanceof Position))
+			return false;
+		else
+			return((this.getXPosition() == ((Position)other).getXPosition()) &&
+				   (this.getYPosition() == ((Position)other).getYPosition()) &&
+				   (this.getWorld() == ((Position)other).getWorld()));
+	}
+	
+	/**
+	 * Return the hash code for this position.
+	 * 
+	 * @return	...
+	 * 			| result == (int)Math.floor(getXPosition()+getYPosition())
+	 */
+	@Override
+	public int hashCode() {
+		return (int)Math.floor(getXPosition()+getYPosition());
+	}
+	
+	/**
+	 * Return a textual representation for this position.
+	 * 
+	 * @return	...
+	 * 			| result.contains("x: ")
+	 * @return	...
+	 * 			| result.contains(String.valueOf(getDisplayedXPosition()))
+	 * @return	...
+	 * 			| result.contains("y: ")
+	 * @return	...
+	 * 			| result.contains(String.valueOf(getDisplayedYPosition()))
+	 */
 	@Override
 	public String toString(){
-		return "x: " + getDisplayedXPosition() + ", y: " + getDisplayedYPosition();
+		return "x: " + String.valueOf(getDisplayedXPosition()) + ", y: " + 
+				String.valueOf(getDisplayedYPosition());
 	}
 }
