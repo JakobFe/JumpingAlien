76f494a00b2e84e456677d47e3f047bb01872ef2
Jakob Festraets <jaapjop95@gmail.com>
2015-05-09 10:35:55 +0200

    Subtraction and Division added, experiment modified.

---
+++ src/jumpingalien/model/program/Subtraction.java
@@ -0,0 +1,36 @@
+package jumpingalien.model.program;
+
+
+import jumpingalien.part3.programs.SourceLocation;
+import java.lang.Double;
+
+public class Subtraction extends BinaryOperator {
+
+	public Subtraction(SourceLocation sourceLocation, 
+			Double leftOperand, 
+			Double rightOperand){
+		super(sourceLocation,leftOperand,rightOperand);
+	}
+	
+	@Override
+	public Double getLeftOperand() {
+		return (Double) super.getLeftOperand();
+	}
+	
+	@Override
+	public Double getRightOperand() {
+		return (Double) super.getRightOperand();
+	}
+	
+	@Override
+	public String getOperatorSymbol() {
+		return "-";
+	}
+
+	@Override
+	public Number outcome() {
+		return getLeftOperand().doubleValue() - 
+			   getRightOperand().doubleValue(); 
+	}
+
+}
