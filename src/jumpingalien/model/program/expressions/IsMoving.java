abe54905a575940741a47a21c23424d19d91b3a3
Jakob Festraets <jaapjop95@gmail.com>
2015-05-11 15:47:06 +0200

    Optimized outcome function in isMoving.

--- src/jumpingalien/model/program/expressions/IsMoving.java
+++ src/jumpingalien/model/program/expressions/IsMoving.java
@@ -27,25 +27,7 @@ public class IsMoving extends UnaryOperator {
 	@Override
 	public Boolean outcome() {
 		jumpingalien.model.game.Direction theDirection =
-				convertDirection(getDirection());
+				jumpingalien.model.game.Direction.convertDirection(getDirection());
 		return ((GameObject) getOperand().outcome()).isMoving(theDirection);
 	}
-	
-	
-	public jumpingalien.model.game.Direction convertDirection(
-			Constant<jumpingalien.part3.programs.IProgramFactory.Direction> dir)
-			throws IllegalArgumentException{
-		// TO DO
-		if (dir.outcome() == jumpingalien.part3.programs.IProgramFactory.Direction.LEFT)
-			return jumpingalien.model.game.Direction.LEFT;
-		else if (dir.outcome() == jumpingalien.part3.programs.IProgramFactory.Direction.RIGHT)
-			return jumpingalien.model.game.Direction.RIGHT;
-		else if (dir.outcome() == jumpingalien.part3.programs.IProgramFactory.Direction.DOWN)
-			return jumpingalien.model.game.Direction.DOWN;
-		else if (dir.outcome() == jumpingalien.part3.programs.IProgramFactory.Direction.UP)
-			return jumpingalien.model.game.Direction.UP;
-		else
-			throw new IllegalArgumentException();
-	}
-
 }
