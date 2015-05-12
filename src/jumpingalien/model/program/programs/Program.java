05e8a2a9dfbb342b439fb43e37a9b9648953520c
Vincent Kemps <kempsvincent@gmail.com>
2015-05-12 12:19:09 +0200

    started implementation of Program.

--- src/jumpingalien/model/program/programs/Program.java
+++ src/jumpingalien/model/program/programs/Program.java
@@ -1,5 +1,28 @@
 package jumpingalien.model.program.programs;
 
-public class Program {
+import java.util.Map;
 
+import jumpingalien.model.program.expressions.Variable;
+import jumpingalien.model.program.statements.Statement;
+
+public class Program {
+	
+	public Program(Statement mainStatement,
+			Map<String, Variable> globalVariables){
+		this.mainStatement = mainStatement;
+		this.globalVariables = globalVariables;
+	}
+	
+	public Statement getMainStatement() {
+		return mainStatement;
+	}
+
+	private final Statement mainStatement;
+	
+	public Map<String, Variable> getGlobalVariables() {
+		return globalVariables;
+	}
+
+	private final Map<String, Variable> globalVariables;
+	
 }
