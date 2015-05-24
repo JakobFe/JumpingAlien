6582b1b357940cd6644b7398d02f8bfd352e495c
Vincent Kemps <kempsvincent@gmail.com>
2015-05-24 14:28:49 +0200

    Completed documentation for JumpInterface

--- src/jumpingalien/model/game/JumpInterface.java
+++ src/jumpingalien/model/game/JumpInterface.java
@@ -1,8 +1,26 @@
 package jumpingalien.model.game;
 
+/**
+ * An interface that allows game objects to jump.
+ * 
+ * @author 	Jakob Festraets, Vincent Kemps
+ * @version	1.0 
+ * 
+ */
 public interface JumpInterface {
 	
+	/**
+	 * A method to start jumping.
+	 */
 	void startJump();
+	
+	/**
+	 * A method to end jumping.
+	 */
 	void endJump();
+	
+	/**
+	 * Returns whether or not this object is jumping.
+	 */
 	boolean isJumping();
 }
